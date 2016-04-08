5ddca145b3a85e1055a9f56b8bd8dcea3c809e31
Cory Parsons <babylonstudio@gmail.com>
2016-04-07 21:07:16 -0400

    use a qthread instead of wakeup cb

--- mpv-pyqt5.py
+++ mpv-pyqt5.py
@@ -1,53 +1,69 @@
 import sys
 import os
-from PyQt5.QtWidgets import QWidget, QMainWindow, QAction, QMenu, QApplication, QHBoxLayout, QSlider, QFileDialog
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QObject, QPoint
-import mpv
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+from PyQt5.QtWidgets import (QWidget, QMainWindow, QAction, QMenu, QApplication,
+                             QHBoxLayout, QSlider, QFileDialog)
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QObject, QPoint, QThread
+import mpv
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+class EventThread(QThread):
+    mpv_event = pyqtSignal(dict)
+
+    def __init__(self, mpv_instance, parent=None):
+        super().__init__(parent)
+        self.mpv = mpv_instance
+
+    def run(self):
+        logging.debug('Event loop: starting.')
+        while True:
+            try:
+                event = self.mpv.wait_event(timeout=-1)
+                devent = event.as_dict()
+            except Exception as e:
+                logging.debug('Event loop: ' + str(e))
+                break
+            if devent['event_id'] == mpv.MpvEventID.NONE:
+                logging.debug('Event loop: None event.')
+                break
+            elif devent['event_id'] == mpv.MpvEventID.SHUTDOWN:
+                logging.debug('Event loop: Shutdown event.')
+                self.mpv.set_wakeup_callback(None)
+                self.mpv.detach_destroy()
+                self.mpv = None
+                break
+            self.mpv_event.emit(devent)
 
 
 class QMpv(QObject):
-    mpv_event = pyqtSignal()
+    wakeup = pyqtSignal()
     new_duration = pyqtSignal(int)
     playback_time = pyqtSignal(int)
 
     def __init__(self, wid, parent=None):
         super().__init__(parent)
-        self.mpv_event.connect(self.handle_events, type=Qt.QueuedConnection)
         self.mpv = mpv.MPV(wid=wid)
         self.mpv.observe_property('track-list', mpv.MpvFormat.NODE)
         self.mpv.observe_property('playback-time', mpv.MpvFormat.DOUBLE)
         self.mpv.observe_property('duration', mpv.MpvFormat.DOUBLE)
 
-        self.mpv.set_wakeup_callback(self.wakeup_cb)
+        self.event_loop = EventThread(self.mpv)
+        self.wakeup.connect(self.event_loop.start)
+        self.event_loop.mpv_event.connect(self.handle_event)
+        self.wakeup.emit()
 
     def quit(self):
-        self.mpv.set_wakeup_callback(None)
+        logging.debug('quit')
         self.mpv.quit()
-
-    def wakeup_cb(self, ctx):
-        self.mpv_event.emit()
-
-    @pyqtSlot()
-    def handle_events(self):
-        while True:
-            if self.mpv is None:
-                break
-            try:
-                event = self.mpv.wait_event(timeout=0)
-                devent = event.as_dict()
-            except Exception as e:
-                logging.debug(e)
-                break
-            if devent['event_id'] == mpv.MpvEventID.NONE:
-                break
-            self.handle_event(devent)
+        self.event_loop.wait()
+        self.mpv = None
 
     def handle_event(self, event):
         handler = {
             mpv.MpvEventID.IDLE: self.on_idle,
-            mpv.MpvEventID.SHUTDOWN: self.on_shutdown,
             mpv.MpvEventID.START_FILE: self.on_start_file,
             mpv.MpvEventID.END_FILE: self.on_end_file,
             mpv.MpvEventID.PAUSE: self.on_pause,
@@ -62,10 +78,6 @@ class QMpv(QObject):
 
     def on_idle(self, event):
         pass
-
-    def on_shutdown(self, event):
-        self.mpv.detach_destroy()
-        self.mpv = None
 
     def on_start_file(self, event):
         pass
@@ -97,12 +109,15 @@ class QMpv(QObject):
     def on_metadata_update(self, event):
         pass
 
+    @pyqtSlot(str)
     def play(self, path):
         self.mpv.play(path)
 
+    @pyqtSlot(int)
     def seek_to(self, ms):
         self.mpv.command_node('seek', ms / 1000.0, 'absolute+exact')
 
+    @pyqtSlot(int)
     def skip(self, ms):
         self.mpv.command_node('seek', ms / 1000.0, 'relative+exact')
 
@@ -111,7 +126,7 @@ class PlayerControls(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        layout = QHBoxLayout()
+        layout = QHBoxLayout(self)
         self.seek_bar = QSlider(orientation=Qt.Horizontal, parent=self)
         layout.addWidget(self.seek_bar)
         self.setLayout(layout)
@@ -137,7 +152,7 @@ class Player(QMainWindow):
         self.mpv_container.setAttribute(Qt.WA_NativeWindow)
         wid = int(self.mpv_container.winId())
 
-        self.mpv = QMpv(wid=wid)
+        self.mpv = QMpv(wid)
 
         self.controller = PlayerControls()
         self.controller.show()
@@ -159,11 +174,12 @@ class Player(QMainWindow):
         self.mpv.seek_to(self.controller.seek_bar.value())
 
     def closeEvent(self, event):
-        self.mpv.quit()
         self.controller.close()
+        self.mpv.quit()
 
 
 if __name__ == '__main__':
+    os.environ['LC_NUMERIC'] = 'C'
     app = QApplication(sys.argv)
     window = Player()
     window.show()
