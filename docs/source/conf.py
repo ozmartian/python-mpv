412c4257a23ee6ebbbe288ab786fe800d78c3f54
Cory Parsons <babylonstudio@gmail.com>
2016-07-21 14:28:28 -0400

    doc fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,11 @@
 import sys
 import os
 
-import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('../../'))
 from mpv import __version__
+
+import sphinx_rtd_theme
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
